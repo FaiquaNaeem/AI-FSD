9de5bafd8debe7d2597f562340e75ef1d6022a94
java.146 <preetisinghthakur07@gmail.com>
2026-03-12 10:33:46 +0530

    First commit

--- MSE-1/backend/server.js
+++ MSE-1/backend/server.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const connectDB = require('./config/db')
-const patientRoutes = require('./routes/patientRoutes')
+const bookRoutes = require('./routes/bookRoutes')
 const errorHandler = require('./middleware/errorMiddleware')
 
 const app =express()
 app.use(express.json())
 connectDB()
-app.use('/patients',patientRoutes)
+app.use('/books',bookRoutes)
 app.use(errorHandler)
 const PORT = process.env.PORT || 5000
 app.listen(PORT,()=>{
